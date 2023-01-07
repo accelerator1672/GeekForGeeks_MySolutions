b4581da7d1c231bd0c076bc76f01dd4a90d35766
tenguwarrior24 <121617251+tenguwarrior24@users.noreply.github.com>
2023-01-07 14:00:49 +0900

    Update BiTree_VerticalTraversal.cpp

--- BiTree_VerticalTraversal.cpp
+++ BiTree_VerticalTraversal.cpp
@@ -4,22 +4,28 @@ class Solution
 {
     public:
     //Function to find the vertical order traversal of Binary Tree.
-    int tag_angle(Node * node, int angle, map<Node*, int> &node_angle){
+    int tag_angle(Node * node, int angle, int *ptr1, int *ptr2, unordered_map<Node*, int> &node_angle){
         node_angle[node] = angle;
         
-        if(node->left){tag_angle(node->left, angle-1, node_angle);}
-        if(node->right){tag_angle(node->right, angle+1, node_angle);}
+        if(angle < *ptr1){*ptr1 = angle;}
+        else if(angle > *ptr2){*ptr2 = angle;}
+        
+        if(node->left){tag_angle(node->left, angle-1, ptr1, ptr2, node_angle);}
+        if(node->right){tag_angle(node->right, angle+1, ptr1, ptr2, node_angle);}
     }
     
-    void bfs(Node *root, map<int,vector<int>> &mymap, map<Node*,int> &node_angle){
+    void bfs(Node *root, int offset, vector<vector<int>> &vec, unordered_map<Node*,int> &node_angle){
+        
         int angle;
         queue<Node*> myqueue;
         myqueue.push(root);
+        
         while(!myqueue.empty()){
+            
             Node* node = myqueue.front();
             myqueue.pop();
             angle = node_angle[node];
-            mymap[angle].push_back(node->data);
+            vec[angle+offset].push_back(node->data);
             
             if(node->left){myqueue.push(node->left);}
             if(node->right){myqueue.push(node->right);}
@@ -28,19 +34,25 @@ class Solution
     
     vector<int> verticalOrder(Node *root)
     {
+      
         vector<int>result;
-        map<Node*, int> node_angle;
-        map<int, vector<int>> mymap;
+        unordered_map<Node*, int> node_angle;
+        int min = 0, max = 0;
         
-        tag_angle(root, 0, node_angle);
-
-        bfs(root, mymap, node_angle);
+        tag_angle(root, 0, &min, &max, node_angle);
+        int size = max-min+1;
+        int offset = - min;
         
-        for(map<int,vector<int>>::iterator it = mymap.begin(); it!= mymap.end(); ++it){
-            for(vector<int>::iterator u = it->second.begin(); u != it->second.end(); ++u){
-                result.push_back(*u);
+        vector<vector<int>> vec(size);
+        
+        bfs(root, offset, vec, node_angle);
+        
+        for(auto row : vec){
+            for(auto col: row){
+                result.push_back(col);
             }
         }
         return result;
+        
     }
 };
