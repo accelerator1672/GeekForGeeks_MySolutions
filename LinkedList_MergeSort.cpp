57e6b41e7e207afbbb5331bd8fd397bc49b3e08c
tenguwarrior24 <121617251+tenguwarrior24@users.noreply.github.com>
2023-01-07 16:15:58 +0900

    Update LinkedList_MergeSort.cpp

--- LinkedList_MergeSort.cpp
+++ LinkedList_MergeSort.cpp
@@ -1,21 +1,4 @@
-
-//https://practice.geeksforgeeks.org/problems/sort-a-linked-list/1?page=1&difficulty[]=1&difficulty[]=2&category[]=Linked%20List&sortBy=submissions
-
-//{ Driver Code Starts
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node* next;
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
+//https://practice.geeksforgeeks.org/problems/sort-a-linked-list/1
 
 class Solution{
   public:
@@ -78,40 +61,3 @@ class Solution{
         return merged;
     }
 };
-
-
-//{ Driver Code Starts.
-
-void printList(Node* node) {
-    while (node != NULL) {
-        printf("%d ", node->data);
-        node = node->next;
-    }
-    printf("\n");
-}
-
-void push(struct Node** head_ref, int new_data) {
-    Node* new_node = new Node(new_data);
-
-    new_node->next = (*head_ref);
-    (*head_ref) = new_node;
-}
-
-int main() {
-    long test;
-    cin >> test;
-    while (test--) {
-        struct Node* a = NULL;
-        long n, tmp;
-        cin >> n;
-        for (int i = 0; i < n; i++) {
-            cin >> tmp;
-            push(&a, tmp);
-        }
-        Solution obj;
-        a = obj.mergeSort(a);
-        printList(a);
-    }
-    return 0;
-}
-// } Driver Code Ends
