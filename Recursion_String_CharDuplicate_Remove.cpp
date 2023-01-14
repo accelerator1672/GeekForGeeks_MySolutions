17ed7ac9d837c46cf20a3205e7552328a4344c08
tenguwarrior24 <121617251+tenguwarrior24@users.noreply.github.com>
2023-01-14 16:02:41 +0900

    Create Recursion_String_CharDuplicate_Remove.cpp

---
+++ Recursion_String_CharDuplicate_Remove.cpp
@@ -0,0 +1,35 @@
+class Solution{
+public:
+
+    string remove(string s, int n){
+
+       int len = s.length(), i = 1;
+       bool changed = 0;
+       
+       if(len <=1 ){return s;}
+       
+       while( i < len){
+           if(s[i] == s[i-1]){
+               if(!changed){changed = 1;}
+               
+               if(i < len-1 && s[i] == s[i+1]){
+                   s.erase(i, 1);
+                   len-- ;
+               }else{
+                   s.erase(i-1, 2);
+                   len-=2;
+               }
+           }
+           else{ i++; }
+       }
+       
+       if(changed){return remove(s,1);}
+      
+       return s;
+    }
+    
+    
+    string rremove(string s){
+        return remove(s, 1);
+    }
+};
