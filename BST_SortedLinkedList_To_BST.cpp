325ba4c5eb10fa5055c609a510afcc66aeb2d177
tenguwarrior24 <121617251+tenguwarrior24@users.noreply.github.com>
2023-01-15 15:59:18 +0900

    Update BST_SortedLinkedList_To_BST.cpp

--- BST_SortedLinkedList_To_BST.cpp
+++ BST_SortedLinkedList_To_BST.cpp
@@ -25,9 +25,40 @@ struct TNode
         left=right=NULL;
     }
 }; */
+
 class Solution{
   public:
-    TNode* Build_BST(vector<int> &vec, int l, int r ){
+    TNode* Build_BST(LNode** node, int ncount){
+        
+        if(ncount == 0){return NULL;} // if there are no more nodes to build subtrees
+        
+        TNode* left = Build_BST(node, ncount/2);
+        
+        TNode* parent = new TNode((*node)->data);
+        *node = (*node)->next;
+        
+        TNode* right = Build_BST(node, ncount - ncount/2 - 1);
+        
+        parent->left = left;
+        parent->right = right;
+        
+        return parent;
+        
+        
+    }
+    TNode* sortedListToBST(LNode *head){
+        
+        int ncount = 0;
+        
+        for(LNode* node = head; node != NULL; node = node->next){ ncount++; }
+        
+        return Build_BST(&head, ncount);    
+       
+     }
+  
+  // HERE IS ALTERNATIVE METHOD USING ARRAY INDEX
+  
+    /*TNode* Build_BST(vector<int> &vec, int l, int r ){
         if(r-l ==1 ){
             TNode* left = new TNode(vec[l]);
             TNode* parent = new TNode(vec[r]);
@@ -52,5 +83,5 @@ class Solution{
  
        return Build_BST(vec, 0, vec.size()-1);
        
-    }
+    }*/
 };
