df4e56ae84b94682df219892845cff83febbb9fd
tenguwarrior24 <121617251+tenguwarrior24@users.noreply.github.com>
2023-01-17 01:48:34 +0900

    Update Graph_WordBoggle.cpp

--- Graph_WordBoggle.cpp
+++ Graph_WordBoggle.cpp
@@ -4,7 +4,7 @@ public:
     typedef set<string> SET;
     
     void dfs(int r, int c, string str, vector<vector<char>> &board, vector<vector<int>> &vis, 
-    U_SET &dict, U_SET &growth, SET &res){
+    U_SET &dict, U_SET &valid, U_SET &res, vector<string> &result){
         
         int row = board.size();
         int col = board[0].size();
@@ -15,59 +15,48 @@ public:
         vis[r][c] = 1;
         str += board[r][c];
         
-        if(!growth.count(str)){vis[r][c] = 0; return;}
-        if(dict.count(str) && !res.count(str)){res.insert(str);}
+        if(!valid.count(str)){vis[r][c] = 0; return;}
+        if(dict.count(str) && !res.count(str)){res.insert(str); result.push_back(str);}
   
         for(int i = 0; i < 8; i++ ){
             r1 = r + delta_r[i];
             c1 = c + delta_c[i];
             if(r1 >= 0 && r1 < row && c1 >= 0 && c1 < col && !vis[r1][c1]){
-                dfs(r1, c1, str,  board, vis, dict, growth, res);
+                dfs(r1, c1, str,  board, vis, dict, valid, res, result);
       
             }
         }
         vis[r][c] = 0;
-        
     }
-	vector<string> wordBoggle(vector<vector<char> >& board, vector<string>& dictionary) {
+    
+    vector<string> wordBoggle(vector<vector<char> >& board, vector<string>& dictionary) {
 	    
-	    vector<string> result; 
-	    
-	    U_SET dict;
-	    U_SET growth;
-        SET res;
-        unordered_set<char> s;
+	vector<string> result; 
+	U_SET dict, valid, res;
 
         int row = board.size();
         int col = board[0].size();
     
         string str = "";
         
-	    for(auto x : dictionary){
-	        s.insert(x[0]);
-	        dict.insert(x);
-	        for(int i= 0; i<x.length(); i++){
-	            str+=x[i];
-	            growth.insert(str);
-	        }
-	        str = "";
+	for(auto x : dictionary){
+	    dict.insert(x);
+	    for(int i= 0; i<x.length(); i++){
+	        str+=x[i];
+	        valid.insert(str);
 	    }
-	    
-	    vector<vector<int>> visited(row, vector<int>(col, 0));
-	    
-	    for(int r = 0; r < row; r ++){
-	        for (int c = 0; c < col; c++){
-	            if(s.count(board[r][c])){
-	                str = "";
-	                dfs(r, c, str, board, visited, dict, growth, res);
-	            }
-	        }
-	    }
-	    for(set<string> :: iterator it = res.begin(); it!=res.end(); ++it){
-	        result.push_back(*it);
-	    }
-	    
-	    return result;
-	    
+            str = "";
 	}
+	    
+	vector<vector<int>> visited(row, vector<int>(col, 0));
+	    
+	for(int r = 0; r < row; r ++){
+	    for (int c = 0; c < col; c++){ 
+	    	str = "";
+	 	dfs(r, c, str, board, visited, dict, valid, res, result);
+	    }
+	}
+	    
+	return result; 
+    }
 };
